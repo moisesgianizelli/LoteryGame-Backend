d892e643e805d410eba2529c6a3fd343e2175bc9
Moises Gianizelli <78654020+moisesgianizelli@users.noreply.github.com>
2022-06-08 00:22:39 +0100

    Add files via upload

---
+++ java/br/com/caelum/vraptor/controller/DashboardController.java
@@ -0,0 +1,25 @@
+package br.com.caelum.vraptor.controller;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.model.User;
+
+@Path("dashboard")
+@Controller
+public class DashboardController {
+	
+	
+	@Get("")
+	public void dashboard() {
+		
+	}
+	
+
+
+}
